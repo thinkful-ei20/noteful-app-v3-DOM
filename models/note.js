d235e8a487216cc80d44961d825b5f5df0bee2a5
mckoyd <dominick.mckoy@gmail.com>
2018-05-01 14:40:41 -0400

    Added models for noteSchema

---
+++ models/note.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: String,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+const Note = mongoose.model('Note', noteSchema);
+module.exports = {Note};
