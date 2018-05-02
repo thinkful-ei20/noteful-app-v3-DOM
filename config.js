4660f1b1fba61237ffa1b0251a09ed430177b99b
mckoyd <dominick.mckoy@gmail.com>
2018-05-01 20:53:03 -0400

    Changed config to use online db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 8080;
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
+exports.MONGODB_URI = 'mongodb://dev:dev@ds211440.mlab.com:11440/noteful-app' || 'mongodb://localhost/noteful';
